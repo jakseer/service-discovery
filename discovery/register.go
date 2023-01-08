5eaef5fd286547116c3c01bf9cb109695c3f79a9
jakseer <jakseer666@gmail.com>
2023-01-08 14:00:00 +0800

    Init

---
+++ discovery/register.go
@@ -0,0 +1,30 @@
+package discovery
+
+import "context"
+
+type ServiceInstance struct {
+	ID        string              `json:"id"`
+	Name      string              `json:"name"`
+	Endpoints []*EndPointInstance `json:"endpoints"`
+}
+
+type EndPointInstance struct {
+	Endpoint string `json:"endpoint"`
+	Weight   int32  `json:"weight"`
+}
+
+type Watcher interface {
+	Watch(ctx context.Context) <-chan struct{}
+
+	Stop() error
+}
+
+type Registrar interface {
+	Register(ctx context.Context, service *ServiceInstance) error
+
+	Deregister(ctx context.Context, service *ServiceInstance) error
+
+	GetService(ctx context.Context, serviceName string) ([]*ServiceInstance, error)
+
+	Watcher(ctx context.Context, serviceName string) (Watcher, error)
+}
