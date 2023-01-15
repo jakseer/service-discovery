51dbe4c41abbfea464ea88c96f01d0c6908d5f85
jakseer <jakseer666@gmail.com>
2023-01-15 10:35:00 +0800

    Add etcd register

---
+++ discovery/etcd/registry.go
@@ -0,0 +1,121 @@
+package etcd
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"service-discovery/discovery"
+	"time"
+
+	clientv3 "go.etcd.io/etcd/client/v3"
+)
+
+var (
+	_ discovery.Registrar = (*Registry)(nil)
+)
+
+// Option is etcd registry option.
+type Option func(o *options)
+
+type options struct {
+	namespace string
+	ttl       time.Duration
+}
+
+// RegisterTTL register ttl
+func RegisterTTL(ttl time.Duration) Option {
+	return func(o *options) { o.ttl = ttl }
+}
+
+// Namespace registry namespace
+func Namespace(ns string) Option {
+	return func(o *options) { o.namespace = ns }
+}
+
+// Registry is etcd registry.
+type Registry struct {
+	opts   *options
+	client *clientv3.Client
+	lease  clientv3.Lease
+}
+
+// New creates etcd registry
+func New(client *clientv3.Client, opts ...Option) (r *Registry) {
+	op := &options{
+		namespace: "/services",
+		ttl:       time.Second * 15,
+	}
+	for _, o := range opts {
+		o(op)
+	}
+	return &Registry{
+		opts:   op,
+		client: client,
+	}
+}
+
+func (r *Registry) Register(ctx context.Context, service *discovery.ServiceInstance) error {
+	vb, err := json.Marshal(service)
+	if err != nil {
+		return err
+	}
+
+	if r.lease != nil {
+		r.lease.Close()
+	}
+	r.lease = clientv3.NewLease(r.client)
+
+	key := fmt.Sprintf("%s/%s/%s", r.opts.namespace, service.Name, service.ID)
+	leaseID, err := r.registerWithKV(ctx, key, string(vb))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *Registry) Deregister(ctx context.Context, service *discovery.ServiceInstance) error {
+	defer func() {
+		if r.lease != nil {
+			r.lease.Close()
+		}
+	}()
+	key := fmt.Sprintf("%s/%s/%s", r.opts.namespace, service.Name, service.ID)
+	_, err := r.client.Delete(ctx, key)
+	return err
+}
+
+func (r *Registry) GetService(ctx context.Context, name string) ([]*discovery.ServiceInstance, error) {
+	key := fmt.Sprintf("%s/%s", r.opts.namespace, name)
+	resp, err := r.client.Get(ctx, key)
+	if err != nil {
+		return nil, err
+	}
+
+	items := make([]*discovery.ServiceInstance, 0, len(resp.Kvs))
+	for _, kv := range resp.Kvs {
+		var si discovery.ServiceInstance
+		err := json.Unmarshal(kv.Value, &si)
+		if err != nil {
+			return nil, err
+		}
+		items = append(items, &si)
+	}
+	return items, nil
+}
+
+func (r *Registry) Watcher(ctx context.Context, serviceName string) (discovery.Watcher, error) {
+	return newWatcher(ctx, r.opts.namespace, serviceName, r.client)
+}
+
+func (r *Registry) registerWithKV(ctx context.Context, key string, value string) (clientv3.LeaseID, error) {
+	grant, err := r.lease.Grant(ctx, int64(r.opts.ttl.Seconds()))
+	if err != nil {
+		return 0, err
+	}
+	_, err = r.client.Put(ctx, key, value, clientv3.WithLease(grant.ID))
+	if err != nil {
+		return 0, err
+	}
+	return grant.ID, nil
+}
